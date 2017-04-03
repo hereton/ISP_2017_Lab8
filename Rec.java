c97783b1ad14a4172a2b07d4d57786a20aa35706
Triwith <triwith.m@ku.th>
2017-04-03 14:55:21 +0700

    Change reverse method

--- Rec.java
+++ Rec.java
@@ -32,9 +32,10 @@ public class Rec {
     /** = a copy of s with characters in reverse order.
 		Example: reverse("abcdefg") = "gfedcba". */
 	public static String reverse(String s) {
-	
-	// fill me in
-	
+		if(s.length()<2){
+			return s;
+		}
+		return s.charAt(s.length()-1)+reverse(s.substring(0,s.length()-1));
 	}
 
     public static void main(String[] args) {
